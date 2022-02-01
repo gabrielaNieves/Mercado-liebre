1d9dbd48b64a0adb932e12c3220de2d2d0b78298
gabrielaNieves <gabrielapaez95@gmail.com>
2022-02-01 19:11:39 -0300

    Actualizaciones de proyecto

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-app.use(express.static(__dirname + '/public'))
+app.use(express.static('public'));
 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+app.listen(3030, ()=>{
+    console.log('Servidor funcionando');
 });
 
-app.listen(3000, () => console.log('servidor en funcionamiento'));
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
